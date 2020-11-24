1201aade139863810e8beff902ba486e34ef9b1e
Route9Library <you@example.com>
2020-11-24 16:55:26 -0500

    floating up & down

--- prompt.js
+++ prompt.js
@@ -1,30 +1,42 @@
-// Prompt a user twice to input a number. Store their response in variables.
-const num = prompt( "Pick any number ");
-// Declare another variable to store the total of each statement
-const realnum = parseInt(num);
+// // Prompt a user twice to input a number. Store their response in variables.
+// const num = prompt( "Enter any number ");
+// // Declare another variable to store the total of each statement
+// const realnum = parseInt(num);
 
-const num2 = prompt("Pick a second number ");
-const realnum2 = parseInt(num2);
+// const num2 = prompt("Thank you, enter a second number ");
+// const realnum2 = parseInt(num2);
 
-// // Code a series of JS statements that use the user input to preform math operations.
-const addition = realnum + realnum2;
-console.log(addition);
+// // // Code a series of JS statements that use the user input to preform math operations.
+// const addition = realnum + realnum2;
+// console.log(addition);
 
-const subtraction = realnum - realnum2;
-console.log(subtraction);
+// const subtraction = realnum - realnum2;
+// console.log(subtraction);
 
-const multiplication = realnum * realnum2;
-console.log(multiplication);
+// const multiplication = realnum * realnum2;
+// console.log(multiplication);
 
-const division = realnum / realnum2;
-console.log(division);
+// const division = realnum / realnum2;
+// console.log(division);
 
-const exponent = Math.pow(realnum, realnum2)
-console.log(exponent);
+// const exponent = Math.pow(realnum, realnum2)
+// console.log(exponent);
 
-const modulus = (realnum % realnum2)
-console.log(modulus);
+// const modulus = (realnum % realnum2)
+// console.log(modulus);
 
-// Code a statement that would return a random number between 1 and 35.
-const randomNumber = Math.floor( Math.random() * 35 ) + 1 ;
-console.log(randomNumber);
+// // Code a statement that would return a random number between 1 and 35.
+// const randomNumber = Math.floor( Math.random() * 35 ) + 1 ;
+// console.log(randomNumber);
+
+// // Code a statement to return a floating point number rounded up to the nearest integer.
+// const decimal = prompt ('Enter any number with a decimal');
+// const floatUp = parseInt(decimal);
+// const decimalNumber = Math.ceil (Math.random() * floatUp ) + 1;
+// console.log(decimalNumber);
+
+// Code a statement that returns a floating point number rounded down to the nearest integer.
+const number = prompt ('Enter another number with a decimal');
+const floatDown = parseInt(number);
+const floatNumber = Math.floor (Math.random() * floatDown ) + 1;
+console.log(floatNumber);
